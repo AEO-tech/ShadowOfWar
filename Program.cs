edd4f4edd7b0b22ccc3ea85d07c2d0c2e5f0af0e
AEO <Zachary8heaton@gmail.com>
2024-09-03 20:43:23 +0100

    added execution and wrath cheats

--- Program.cs
+++ Program.cs
@@ -11,9 +11,12 @@ namespace ShadowOfWar
         [DllImport("user32.dll")]
         static extern short GetAsyncKeyState(int vKey);
 
-        public bool Arrows = false;
-        public bool Health = false;
+        public bool arrows = false;
+        public bool health = false;
         public bool focus = false;
+        public bool execution = false;
+        public bool wrath = false;
+
         bool ShowWindow = true;
         bool confirm = false;
 
@@ -33,8 +36,10 @@ namespace ShadowOfWar
             Swed swed = new("ShadowOfWar");
             IntPtr moduleBase = swed.GetModuleBase("ShadowOfWar.exe");
             IntPtr Arrow = swed.ReadPointer(moduleBase, 0x02D21140, 0x28, 0x468, 0x18, 0x8, 0x0) + 0xA8;
-            IntPtr health = swed.ReadPointer(moduleBase, 0x02D21140, 0x28, 0x468, 0x20) + 0x42C;
+            IntPtr Health = swed.ReadPointer(moduleBase, 0x02D21140, 0x28, 0x468, 0x20) + 0x42C;
             IntPtr Focus = swed.ReadPointer(moduleBase, 0x02D21140, 0x28, 0x468, 0x8, 0x0, 0x0, 0x0) + 0x224;
+            IntPtr Execution = swed.ReadPointer(moduleBase, 0x02D1C9F8, 0x70, 0x48, 0x10) + 0x2C8;
+            IntPtr Wrath = swed.ReadPointer(moduleBase, 0x02D21140, 0xE8, 0x28, 0x18, 0x8) + 0x308;
 
             if (ShowWindow)
             {
@@ -50,6 +55,7 @@ namespace ShadowOfWar
                         confirm = true;
                     }
                 }
+                else confirm = true;
 
                 if (confirm == true)
                 {
@@ -60,18 +66,23 @@ namespace ShadowOfWar
                     {
                         if (ImGui.BeginTabItem("Cheats"))
                         {
-                            ImGui.Checkbox("Infinite Arrows", ref Arrows);
+                            ImGui.Checkbox("GodMode", ref health);
+                            ImGui.Checkbox("Infinite Arrows", ref arrows);
                             ImGui.Checkbox("Infinite Focus", ref focus);
-                            ImGui.Checkbox("GodMode", ref Health);
+                            ImGui.Checkbox("Infinite Execution",ref execution);
+                            ImGui.Button(("give max Wrath"));
+                                wrath = true;
                             ImGui.EndTabItem();
                         }
 
                         if (ImGui.BeginTabItem("Stats"))
                         {
                             ImGui.Text("Player Stats:");
-                            ImGui.Text($"Health = {swed.ReadFloat(health).ToString()}");
+                            ImGui.Text($"Health = {swed.ReadFloat(Health).ToString()}");
                             ImGui.Text($"Arrows = {swed.ReadInt(Arrow).ToString()}");
                             ImGui.Text($"Focus = {swed.ReadFloat(Focus).ToString()}");
+                            ImGui.Text($"Execution = {swed.ReadFloat(Execution).ToString()}");
+                            ImGui.Text($"Wrath = {swed.ReadFloat(Wrath).ToString()}");
                             ImGui.EndTabItem();
                         }
 
@@ -104,14 +115,23 @@ namespace ShadowOfWar
                     }
                     ImGui.End();
 
-                    if (Arrows)
+                    if (arrows)
                         swed.WriteBytes(Arrow, "90 90 90 90 90 90");
 
-                    if (Health)
-                        swed.WriteFloat(health, 100000F);
+                    if (health)
+                        swed.WriteFloat(Health, 100000F);
 
                     if (focus)
                         swed.WriteFloat(Focus, 10000F);
+
+                    if (execution)
+                        swed.WriteFloat(Execution, 500F);
+
+                    if (wrath)
+                    {
+                        swed.WriteFloat(Wrath, 99F);
+                        wrath = false;
+                    }
                 }
             }
         }
